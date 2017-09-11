5e10b61d7568cb84001c5abd4ef40ea9f1d3d791
Wu Jian Ping <wujjpp@hotmail.com>
2017-09-11 19:05:39 +0800

    .

--- src/create-router.js
+++ src/create-router.js
@@ -4,13 +4,13 @@
 
 import Vue from 'vue'
 import Router from 'vue-router'
-import routes from './routes'
+import routers from './routers'
 
 Vue.use(Router)
 
 export default () => {
   return new Router({
     mode: 'history',
-    routes: routes
+    routes: routers
   })
 }
