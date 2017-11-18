e9619f61c251cdf5c8d6f6e0142e0afc9f5b78dc
Wu Jian Ping <wujjpp@hotmail.com>
2017-11-18 11:39:37 +0800

    rename app

---
+++ src/components/app/component.js
@@ -0,0 +1,10 @@
+import '../../styles/global.scss'
+import './style.scss'
+
+import navBar from '../nav-bar/index.vue'
+
+export default {
+  components: {
+    'nav-bar': navBar
+  }
+}
---
+++ src/components/app/index.vue
@@ -0,0 +1,8 @@
+<template>
+<div>
+  <nav-bar />
+  <router-view></router-view>
+</div>
+</template>
+
+<script src="./component.js"></script>
